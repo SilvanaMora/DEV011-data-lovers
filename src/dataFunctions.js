641a5bf5b0de6ecdc2e714534115c9db4c4e9632
AngRM <angelicariosmar@gmail.com>
2023-09-19 20:45:31 -0600

    union portada1

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,50 +1,9 @@
-export const sortData = (ordenar) => {
-  const copia = [...ordenar]
-  return copia.sort ((dataBanderasA, dataBanderasB) => {
-    if (dataBanderasA.name > dataBanderasB.name) {
-      return 1;
-    }
-    else if (dataBanderasA.name < dataBanderasB.name){
-      return -1;
-    } else {
-      return 0;
-    }
-  });
-}; 
+// estas funciones son de ejemplo
 
-function filtrar (data, continente){
-  const copia1 = [...data]
-  const continents = copia1.filter((elemento)=>{
-    if (elemento.continents.includes(continente))
-    {
-      return true;
-    } else {
-      return false
-    }
-  })
-  return continents
-}
+export const example = () => {
+  return 'example';
+};
 
-function filtrar1 (data, suBregion){
-  const copia2 = [...data]
-  const subregion = copia2.filter((elemento)=>{
-    if (elemento.subregion.includes(suBregion))
-    {
-      return true;
-    } else {
-      return false
-    }
-  })
-  return subregion
-}
-
-export const calcular = (data) =>{
-  const copia3 = [...data]
-  let porcentaje = copia3.length;
-  porcentaje = Math.round((porcentaje * 100 )/200);
-  const spanValor = document.getElementById("ponerValor")
-  spanValor.textContent = porcentaje + "%";
-}
-
-export {
-  filtrar, filtrar1} 
+export const anotherExample = () => {
+  return [];
+};
--- src/main.js
+++ src/main.js
@@ -1,19 +1,31 @@
 import data from './data/countries/countries.js';
 import { renderItems } from "./view.js";
 
+
+document.querySelector("#inicio").addEventListener("click", function() {
+  document.querySelector("#portada").style.opacity = 0;
+
+  
+  document.querySelector("#tituloPag2").style.opacity = 1;
+  document.querySelector("#contenedorDeFormulario").style.opacity = 1;
+  document.querySelector("#contenedorDeFiltrado").style.opacity = 1;
+  document.querySelector("#contenedorDeCartas").style.opacity = 1;
+  document.querySelector("#piePagina").style.opacity = 1;
+});
+
 const allData = renderItems(data.countries)
 document.querySelector('#root').innerHTML = allData
-console.log('hola mundo', );
+
 
 const inputText = document.getElementById("textoIngresado");
 
-const botonBuscar = document.getElementById("Buscar");
+const botonBuscar = document.getElementById("buscar");
 botonBuscar.addEventListener("click", buscarPais);
 
 function buscarPais() {
   inputText.value = "";
 }
-const botonLimpiar = document.getElementById("Limpiar");
+const botonLimpiar = document.getElementById("limpiar");
 botonLimpiar.addEventListener("click", limpiarTexto);
 function limpiarTexto() {
   inputText.value = ""; // Establece el valor del input en una cadena vacía para borrar el texto.
