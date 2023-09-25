76a87d66122ec6366eca07b7a647c5338ed7099b
AngRM <angelicariosmar@gmail.com>
2023-09-25 08:18:42 -0600

    busquedaPaises

--- src/main.js
+++ src/main.js
@@ -7,23 +7,43 @@ document.getElementById("inicio").onclick = function(){
   document.getElementById("portada").style.display = "none";
 	document.getElementById("contenedorFunciones").style.display = null;
   document.getElementById("fotter").style.display = null;
+  document.getElementById("contenedorBotones").style.display = null;
+
 }
 
 const allData = renderItems(data.countries)
 document.querySelector('#root').innerHTML = allData
 
 
-const inputText = document.getElementById("textoIngresado");
+const textoBuscador = document.getElementById("textoIngresado");
+textoBuscador.addEventListener("keyup", BuscarPais);
 
-// const botonBuscar = document.getElementById("buscar");
-// botonBuscar.addEventListener("click", buscarPais);
 
-function buscarPais() {
-  inputText.value = "";
-}
 const botonLimpiar = document.getElementById("limpiar");
 botonLimpiar.addEventListener("click", limpiarTexto);
 function limpiarTexto() {
   inputText.value = ""; // Establece el valor del input en una cadena vacía para borrar el texto.
 }
+
+document.getElementById("textoIngresado")[0].onkeyup = function(){
+  let text1 = document.getElementsById("textoIngresado")[0].value;
+ BuscarPais.getbuscarPais (text1);
+}
+
+// function BuscarPais() {
+
+function BuscarPais ()  {
+
+    let inputText = document.getElementById("textoIngresado").value.toUpperCase();
+    console.log(inputText);
+    const resultado = data.countries.filter((word) => word.name.common.toUpperCase().includes(inputText));
+  
+    console.log(resultado);
+  const busqueda = renderItems(resultado);
+  document.querySelector('#root').innerHTML = busqueda;
+  }
+  
+
+
+ 
 
