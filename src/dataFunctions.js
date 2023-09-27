5845153adcda562e5bcd2a47cc84a1942f20feb9
AngRM <angelicariosmar@gmail.com>
2023-09-27 10:33:31 -0600

    Hasta la función SortBy

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,3 +1,33 @@
+export function sortData (data, sortBy, sortOrder) {
+  data.sort ((a,b) => {
+      if (a[sortBy].common < b[sortBy].common ) {
+      return -1;
+      }
+      if (a[sortBy].common > b[sortBy].common ) {
+        return 1;
+      }
+    return 0; // En caso de que no se pueda determinar el orden
+  });
+
+console.log (data);
+};
+
+// function BuscarPais() {}
+
+export const BuscarPais = (data) => {
+  let inputText = document.getElementById("textoIngresado").value.toUpperCase();
+  console.log(inputText);
+  const resultado = data.filter((word) => word.name.common.toUpperCase().includes(inputText));
+  return resultado;
+}
+
+
+
+
+
+
+
+
 export function filterData(data, filterBy, selectedValue) {
   const filteredData = data.filter((pais) => {
     return pais[filterBy].includes(selectedValue);
@@ -6,37 +36,6 @@ export function filterData(data, filterBy, selectedValue) {
   return filteredData;
 }
 
-
-
-export const OrdenAlfa = (data, sortBy, sortOrder) => {
-  const paisesArreglo = data;
-  paisesArreglo.sort ((a,b) => {
-    if (a.name > b.name) {
-      return 1;
-    }
-    if (a.name < b.name) {
-      return -1;
-    }
-    
-    return 0;
-  });
-  console.log (paisesArreglo)
-return paisesArreglo
-
-  };
- 
-  export const sortData = (ordenar) => {
-  return "example";
-};
-
-
   export const anotherExample = () => {
   return [];
 };
-// function BuscarPais() {
-
-export const BuscarPais = (data,value) => {
-  const resultado = data.filter((word) => word.name.common.toUpperCase().includes(value.toUpperCase()));
-  return resultado;
-}
-
--- src/main.js
+++ src/main.js
@@ -3,58 +3,43 @@
 import data from './data/countries/countries.js';
 import { renderItems } from "./view.js";
 import { filterData } from "./dataFunctions.js";
-
-
+import { sortData } from "./dataFunctions.js";
+import { BuscarPais } from "./dataFunctions.js";
+console.log(sortData(data.countries, "name"));
 
 document.getElementById("inicio").onclick = function(){
   document.getElementById("portada").style.display = "none";
 	document.getElementById("contenedorFunciones").style.display = null;
   document.getElementById("fotter").style.display = null;
   // document.getElementById("contenedorBotones").style.display = null;
-
 }
 
-const allData = renderItems(data.countries)
-document.querySelector('#root').innerHTML = allData
-
+const allData = renderItems(data.countries);
+document.querySelector('#root').innerHTML = allData;
 
 const textoBuscador = document.getElementById("textoIngresado");
-textoBuscador.addEventListener("keyup", BuscarPais);
+textoBuscador.addEventListener("keyup", pintarBuscarPais);
+limpiar.addEventListener("click", limpiarTexto);
 
 
-const botonLimpiar = document.getElementById("limpiar");
-const inputText = document.getElementById("textoIngresado"); 
-botonLimpiar.addEventListener ("click", function() {
-limpiarTexto (inputText);// Establece el valor del input en una cadena vacía para borrar el texto.
-});
-
-function limpiarTexto(input) { // Acepta input como parámetro
-  input.value = ""; // Establece el valor del input en una cadena vacía para borrar el texto.
+function pintarBuscarPais (){
+const resultado = BuscarPais (data.countries);
+const busqueda = renderItems(resultado);
+document.querySelector('#root').innerHTML = busqueda;
 }
 
-
-
-
-
-
-
-document.getElementById("textoIngresado").onkeyup = function(){
-  let text1 = document.getElementById("textoIngresado").value;
- BuscarPais(text1);
-}
+function limpiarTexto() { 
+    let inputText = document.getElementById("textoIngresado");
+    let inputSelection = document.getElementById("continents");
+    inputText.value = ""; 
+    inputSelection.value = "nada";//Establece el valor del input en una cadena vacía para borrar el texto.
+    const allData1 = renderItems (data.countries);
+    document.querySelector('#root').innerHTML = allData1;
+    console.log ('h',2);
+    };
+ 
 
 // function BuscarPais() {
-
-function BuscarPais ()  {
-
-    let inputText = document.getElementById("textoIngresado").value.toUpperCase();
-    console.log(inputText);
-    const resultado = data.countries.filter((word) => word.name.common.toUpperCase().includes(inputText));
-  
-    console.log(resultado);
-  const busqueda = renderItems(resultado);
-  document.querySelector('#root').innerHTML = busqueda;
-  }
 
 
 // function selectContinents () {
@@ -70,3 +55,7 @@ function BuscarPais ()  {
     );
     root.innerHTML = renderItems(dataFilteredByContinent);
   });
+
+  // function selectContinents () {
+    const root = document.getElementById("root");
+
