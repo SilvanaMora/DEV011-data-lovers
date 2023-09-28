57cb27d56856b1e753ed3c877748902fbb2906c1
AngRM <angelicariosmar@gmail.com>
2023-09-28 10:47:42 -0600

    ya ordena, flata reestablecer al limpiar

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,16 +1,28 @@
-export function sortData (data, sortBy, sortOrder) {
-  data.sort ((a,b) => {
+export function SortData  (data, sortBy, sortOrder)  {
+  const ordenDeDatos = data;
+  ordenDeDatos.sort ((a,b) => {
+    if (sortOrder == "asc") {
       if (a[sortBy].common < b[sortBy].common ) {
-      return -1;
-      }
-      if (a[sortBy].common > b[sortBy].common ) {
-        return 1;
-      }
-    return 0; // En caso de que no se pueda determinar el orden
-  });
+        return -1;
+        }
+        if (a[sortBy].common > b[sortBy].common ) {
+          return 1;
+        }
+      // return 0; // En caso de que no se pueda determinar el orden
+  } else if (sortOrder == "desc") {
+  if (a[sortBy].common > b[sortBy].common ) {
+    return -1;
+    }
+    if (a[sortBy].common < b[sortBy].common ) {
+      return 1;
+    } //return 0; // En caso de que no se pueda determinar el orden
+  }
+});
 
-console.log (data);
+console.log (ordenDeDatos);
+return (ordenDeDatos);
 };
+
 
 // function BuscarPais() {}
 
@@ -20,14 +32,6 @@ export const BuscarPais = (data) => {
   const resultado = data.filter((word) => word.name.common.toUpperCase().includes(inputText));
   return resultado;
 }
-
-
-
-
-
-
-
-
 export function filterData(data, filterBy, selectedValue) {
   const filteredData = data.filter((pais) => {
     return pais[filterBy].includes(selectedValue);
--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@
 import data from './data/countries/countries.js';
 import { renderItems } from "./view.js";
 import { filterData } from "./dataFunctions.js";
-import { sortData } from "./dataFunctions.js";
+import { SortData } from "./dataFunctions.js";
 import { BuscarPais } from "./dataFunctions.js";
-console.log(sortData(data.countries, "name"));
+console.log(SortData(data.countries, "name"));
 
 document.getElementById("inicio").onclick = function(){
   document.getElementById("portada").style.display = "none";
@@ -31,8 +31,10 @@ document.querySelector('#root').innerHTML = busqueda;
 function limpiarTexto() { 
     let inputText = document.getElementById("textoIngresado");
     let inputSelection = document.getElementById("continents");
+    let inputSelectionOrden = document.getElementById("asc-desc");
     inputText.value = ""; 
     inputSelection.value = "nada";//Establece el valor del input en una cadena vacía para borrar el texto.
+    inputSelectionOrden.value = "nada";
     const allData1 = renderItems (data.countries);
     document.querySelector('#root').innerHTML = allData1;
     console.log ('h',2);
@@ -49,13 +51,21 @@ function limpiarTexto() {
     console.log(event.target.value, data);
     const valueSelected = event.target.value;
     const dataFilteredByContinent = filterData(
-      data.countries,
-      "continents",
-      valueSelected
+      data.countries, "continents", valueSelected
     );
     root.innerHTML = renderItems(dataFilteredByContinent);
   });
 
   // function selectContinents () {
     const root = document.getElementById("root");
+
+    const dataOrdenado = data.countries;
+    const selectSort = document.getElementsByName ("asc-desc") [0];
+
+    selectSort.addEventListener("change", () => {
+      let resultadoSort = SortData(dataOrdenado, "name", selectSort.value);
+      const root = document.getElementById("root");
+      root.innerHTML = renderItems(resultadoSort);
+      console.log(resultadoSort);
+    });
 
